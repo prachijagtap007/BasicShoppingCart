231ad7202f9e05542dcaf8109b22b7391224d050
prachijagtap007 <prachij007@gmail.com>
2021-03-24 23:41:22 +0530

    Updates

---
+++ precache-manifest.15e8702bbe59fb0302ae1ac266d5656f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "874da8a69892aa12d8f45b53bdec8d3a",
+    "url": "/BasicShoppingCart/index.html"
+  },
+  {
+    "revision": "1d1e6b16b4f7080dade2",
+    "url": "/BasicShoppingCart/static/css/2.fc3ea6fc.chunk.css"
+  },
+  {
+    "revision": "dce3ba7d4da620a01a46",
+    "url": "/BasicShoppingCart/static/css/main.2ee996cb.chunk.css"
+  },
+  {
+    "revision": "1d1e6b16b4f7080dade2",
+    "url": "/BasicShoppingCart/static/js/2.a794037f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/BasicShoppingCart/static/js/2.a794037f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dce3ba7d4da620a01a46",
+    "url": "/BasicShoppingCart/static/js/main.07073030.chunk.js"
+  },
+  {
+    "revision": "be86cb0a85d010428738",
+    "url": "/BasicShoppingCart/static/js/runtime-main.4052debd.js"
+  }
+]);
